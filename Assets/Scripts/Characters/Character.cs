7965f08e17de39507bd3da51ae4d179dc93f741c
mudyro <michal22.091@gmail.com>
2024-01-08 22:46:24 +0100

    Added group movement mechanics
    Added group movement mechanics
    
    Added large files using lfs

--- Assets/Scripts/Characters/Character.cs
+++ Assets/Scripts/Characters/Character.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Character : MonoBehaviour
 {
@@ -10,13 +12,44 @@ public class Character : MonoBehaviour
 
     public bool isLeader;
 
+    public int positionInFormationIndex;
+
+    RaycastHit _hit = new RaycastHit();
+
+    public NavMeshAgent characterAgent;
+
+    void Awake()
+    {
+        characterAgent = GetComponent<NavMeshAgent>();
+    }
+
     void MoveToMouseClick()
     {
-        //
+        if(Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(_ray,out _hit))
+            {
+                characterAgent.SetDestination(_hit.point);
+            }
+        }
     }
 
     void FollowLeadingCharacter()
     {
-        //
+        characterAgent.SetDestination(FollowingCharactersFormation.formationPositions[positionInFormationIndex]);
     }    
+
+    void FixedUpdate()
+    {
+        if(isLeader)
+        {
+            MoveToMouseClick();
+        }
+        else
+        {
+            FollowLeadingCharacter();
+        }
+    }
 }
--- Assets/Scripts/Characters/FollowingCharactersFormation.cs
+++ Assets/Scripts/Characters/FollowingCharactersFormation.cs
@@ -1,13 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FollowingCharactersFormation : MonoBehaviour
 {
-    public Vector3[] formationPositions;
+    public static Vector3[] formationPositions;
 
-    void CreateNewFormationOfCharacters()
+    [SerializeField] [Range(1,5)]
+    float _distanceToLeader = 1;
+
+    Character[] _allCharacters;
+
+    void Awake()
     {
-        //
+        _allCharacters = FindObjectsOfType<Character>();
+    }
+
+    void Start()
+    {
+        AssignFormationPositions();
+    }
+
+    void AssignFormationPositions()
+    {
+        int positionIndex = 0;
+
+        foreach(Character _character in _allCharacters)
+        {
+            if(!_character.isLeader)
+            {
+                _character.positionInFormationIndex = positionIndex;
+                positionIndex++;
+            }
+        }
+    }
+
+    void CalculateFormationPositions()
+    {
+        formationPositions = new Vector3[_allCharacters.Length - 1];
+
+        Vector3 _leadersVelocityDirection = new();
+        Vector3 _nextCharacterPosition;
+        Character _leader = new();
+
+        foreach(Character _character in _allCharacters)
+        {
+            if(_character.isLeader)
+            {
+                _leader = _character;
+                _leadersVelocityDirection = _character.characterAgent.velocity.normalized;
+            }
+        }
+
+        foreach(Character _character in _allCharacters)
+        {
+            if(!_character.isLeader)
+            {
+                _nextCharacterPosition = _leader.gameObject.transform.position 
+                -_leadersVelocityDirection * _distanceToLeader * (_character.positionInFormationIndex + 1); 
+                formationPositions[_character.positionInFormationIndex] = _nextCharacterPosition;    
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        CalculateFormationPositions();
     }
 }
