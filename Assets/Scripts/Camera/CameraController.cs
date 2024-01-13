c2fdb35035865db71a675b587547ca6111d0c87e
mudyro <michal22.091@gmail.com>
2024-01-13 19:14:08 +0100

    Set up camera

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,16 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     float _cameraToPlayerDistance;
 
-    Transform _leadingCharacterTransform;
+    Character[] _allCharacters;
 
-    void StickCameraToLeadingCharacter()
+    CinemachineVirtualCamera _virtualCamera;
+
+    void Awake()
     {
-        //
+        _virtualCamera = GetComponent<CinemachineVirtualCamera>();
+        _allCharacters = new Character[FindObjectOfType<FollowingCharactersFormation>().gameObject.transform.childCount];
+    }
+
+    void FixedUpdate()
+    {
+        StickCameraToLeadingPlayer();
+    }
+
+    void StickCameraToLeadingPlayer()
+    {
+        if(_allCharacters[0] == null)
+        {
+            _allCharacters = FindObjectOfType<FollowingCharactersFormation>().allCharacters;
+        }
+
+        foreach (Character _character in _allCharacters)
+        {
+            if(_character.isLeader)
+            {
+                _virtualCamera.Follow = _character.gameObject.transform;
+                _virtualCamera.LookAt = _character.gameObject.transform;
+            }
+        }
     }
 
 }
